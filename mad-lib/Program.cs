56a06782856987d25f1a3df91d6f8c60eae556ad
Adam McKnight <adammcknight@me.com>
2020-02-05 18:55:12 -0500

    First Commit

---
+++ mad-lib/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace mad_lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Libby. A Console Madlib Game!");
+
+            string color;
+            Console.WriteLine("Enter a Color –");
+            color = Console.ReadLine();
+
+            string adjOne;
+            Console.WriteLine("Enter a adjitive – ");
+            adjOne = Console.ReadLine();
+
+            string thing;
+            Console.WriteLine("Enter a name something can go through – ");
+            thing = Console.ReadLine();
+
+            string nounOne;
+            Console.WriteLine("Enter a noun – ");
+            nounOne = Console.ReadLine();
+
+            string verbOne;
+            Console.WriteLine("Enter a verb – ");
+            verbOne = Console.ReadLine();
+
+
+            Console.WriteLine("Here's your MadLib...");
+            Console.WriteLine($"The {color} cow was, {adjOne} through the, {thing}; {nounOne} while {verbOne}.");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,10 @@
+## MadLib Console App in .NET
+
+To run this console app on your local machine, follow these steps...
+
+- Clone this repo.
+- `CD` into the coned' project directory
+- Open the `mad-lib.sln` file using Visual Studio on Windows or Mac
+- Run the Console app by pressing F5, or by clicking on run, or using the terminal by using the `dotnet run` command.
+
+
